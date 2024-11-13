8e141de2a3678e6b79b5c7492d1042b03b98d164
yanghy <snxamdf@126.com>
2024-11-13 19:17:04 +0800

    增加 Linux 使用源码安装: aarch64, x86-64

--- src/lazarus.ts
+++ src/lazarus.ts
@@ -476,7 +476,8 @@ export class Lazarus {
 
             let fpcDirname = path.basename(fpcFilename, path.extname(fpcFilename));
             core.info(`Run Install fpc: ${lazarusPath}/${fpcDirname}`);
-            await exec(`./install.sh <<esxu`, [], {cwd: `${lazarusPath}/${fpcDirname}`});
+            await exec(`echo '\n' yes.txt`,[], {cwd: `${lazarusPath}/${fpcDirname}`})
+            await exec(`./install.sh < yes.txt`, [], {cwd: `${lazarusPath}/${fpcDirname}`});
 
             core.info(`Run Install lazarus: ${lazarusPath}`);
             await exec(`make clean all`, [], {cwd: lazarusPath});
