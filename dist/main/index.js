42011ee6f39918637b51fa13a2ace848cb37c59b
yanghy <snxamdf@126.com>
2024-11-13 19:40:20 +0800

    增加 Linux 使用源码安装: aarch64, x86-64

--- dist/main/index.js
+++ dist/main/index.js
@@ -672,8 +672,8 @@ class Lazarus {
             await (0, exec_1.exec)("sudo apt-get install -q -y make binutils build-essential gdb subversion zip unzip libx11-dev libgtk2.0-dev libgdk-pixbuf2.0-dev libcairo2-dev libpango1.0-dev libgtk-3-dev");
             let fpcDirname = path.basename(fpcFilename, path.extname(fpcFilename));
             core.info(`Run Install fpc: ${lazarusPath}/${fpcDirname}`);
-            await (0, exec_1.exec)(`echo '\n' > yes`, [], { cwd: `${lazarusPath}/${fpcDirname}` });
-            await (0, exec_1.exec)(`./install.sh < yes`, [], { cwd: `${lazarusPath}/${fpcDirname}` });
+            fs.writeFileSync(`${lazarusPath}/${fpcDirname}/yes.txt`, "\n");
+            await (0, exec_1.exec)(`./install.sh < yes.txt`, [], { cwd: `${lazarusPath}/${fpcDirname}` });
             core.info(`Run Install lazarus: ${lazarusPath}`);
             await (0, exec_1.exec)(`make clean all`, [], { cwd: lazarusPath });
         }
