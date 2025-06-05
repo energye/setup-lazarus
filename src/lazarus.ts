9958f6c61400dccd124310e49cb24b928a14d515
yanghy <snxamdf@126.com>
2025-06-06 07:59:14 +0800

    增加 MacOS laz 3.8 版本安装

--- src/lazarus.ts
+++ src/lazarus.ts
@@ -255,8 +255,10 @@ export class Lazarus {
             // 解压 Laz IDE zip : sudo unzip lazarus-darwin-aarch64-4.0.zip -d /Applications/
             await exec(`sudo unzip ${downloadPath_DAR} -d /Applications/`);
 
-            // 创建命令 sudo ln -s /Applications/Lazarus/lazbuild /usr/local/bin/lazbuild
+            // 命令 sudo ln -s /Applications/Lazarus/lazbuild /usr/local/bin/lazbuild
             await exec(`sudo ln -s /Applications/lazarus/lazbuild /usr/local/bin/lazbuild`);
+            // sudo ln -s /Applications/Lazarus.app/Contents/MacOS /Developer/lazarus
+            await exec(`sudo ln -s /Applications/Lazarus /Developer/lazarus`);
         } catch (error) {
             throw (error as Error);
         }
